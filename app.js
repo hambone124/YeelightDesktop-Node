dfefbde9fec8616954e8d797b64408fc2baa5696
Hamilton Perez <hpe60m5@gmail.com>
2019-04-12 01:15:22 -0400

    Update package-lock.json

---
+++ app.js
@@ -0,0 +1,19 @@
+const y = require('yeelight-awesome');
+
+const discover = new y.Discover({
+  port: 1982,
+  debug: true,
+});
+discover.once('deviceAdded', (device) => {
+  const yeelight = new y.Yeelight({
+    lightIp: device.host,
+    lightPort: device.port,
+  });
+
+  yeelight.on('connected', () => {
+    yeelight.setRGB(new y.Color(123, 99, 65), 'smooth', 5000);
+  });
+  yeelight.connect();
+});
+
+discover.start();
