5d047828128164f32270c00e67178f9291afd53c
Hamilton Perez <hpe60m5@gmail.com>
2019-04-15 01:37:42 -0400

    Add somewhat functioning code

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
+const ambilight = require('ambilight-provider');
 const y = require('yeelight-awesome');
 
-const discover = new y.Discover({
-  port: 1982,
-  debug: true,
+const yeelight = new y.Yeelight({
+  lightIp: '192.168.1.5',
+  lightPort: '55443',
 });
-discover.once('deviceAdded', (device) => {
-  const yeelight = new y.Yeelight({
-    lightIp: device.host,
-    lightPort: device.port,
-  });
+let red; let green; let blue;
 
-  yeelight.on('connected', () => {
-    yeelight.setRGB(new y.Color(123, 99, 65), 'smooth', 5000);
-  });
-  yeelight.connect();
+ambilight.start(500, (color) => {
+  // Doesn't prevent NaNs
+  if (parseInt(color[0]) !== NaN) {
+    red = parseInt(color[0]);
+    green = parseInt(color[1]);
+    blue = parseInt(color[2]);
+  }
 });
 
-discover.start();
+yeelight.on('connected', () => {
+  console.log('connected');
+});
+
+// Placeholder promise
+const wait = time => new Promise(resolve => setTimeout(resolve, time));
+
+wait(3000).then(() => {
+  yeelight.connect().then((l) => {
+    setInterval(() => {
+      console.log(`${red}, ${green}, ${blue}`);
+      l.setRGB(new y.Color(red, green, blue), 'smooth', 500);
+    }, 1000);
+  }).catch((err) => {
+    console.log('error!');
+    console.error(err);
+  });
+});
+console.log('Starting');
