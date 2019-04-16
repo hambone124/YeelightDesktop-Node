5f76bc411c309a7ad62751d833d31a49d46fc1fa
hambone124 <hpe60m5@gmail.com>
2019-04-15 21:57:45 -0400

    Using yeelight dependency

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const ambilight = require('ambilight-provider');
-const y = require('yeelight-awesome');
+const y = require('yeelight');
 
 const yeelight = new y.Yeelight({
   lightIp: '192.168.1.5',
@@ -8,31 +8,31 @@ const yeelight = new y.Yeelight({
 });
 let red; let green; let blue;
 
-ambilight.start(500, (color) => {
-  // Doesn't prevent NaNs
-  if (!Number.IsNaN(parseInt(color[0], 10))) {
-    red = parseInt(color[0], 10);
-    green = parseInt(color[1], 10);
-    blue = parseInt(color[2], 10);
-  }
-});
+// ambilight.start(500, (color) => {
+//   // Doesn't prevent NaNs
+//   if (!Number.IsNaN(parseInt(color[0], 10))) {
+//     red = parseInt(color[0], 10);
+//     green = parseInt(color[1], 10);
+//     blue = parseInt(color[2], 10);
+//   }
+// });
 
-yeelight.on('connected', () => {
-  console.log('connected');
-});
+// yeelight.on('connected', () => {
+//   console.log('connected');
+// });
 
 // Placeholder promise
-const wait = time => new Promise(resolve => setTimeout(resolve, time));
+// const wait = time => new Promise(resolve => setTimeout(resolve, time));
 
-wait(3000).then(() => {
-  yeelight.connect().then((l) => {
-    setInterval(() => {
-      console.log(`${red}, ${green}, ${blue}`);
-      l.setRGB(new y.Color(red, green, blue), 'smooth', 500);
-    }, 1000);
-  }).catch((err) => {
-    console.log('error!');
-    console.error(err);
-  });
-});
-console.log('Starting');
+// wait(3000).then(() => {
+//   yeelight.connect().then((l) => {
+//     setInterval(() => {
+//       console.log(`${red}, ${green}, ${blue}`);
+//       l.setRGB(new y.Color(red, green, blue), 'smooth', 500);
+//     }, 1000);
+//   }).catch((err) => {
+//     console.log('error!');
+//     console.error(err);
+//   });
+// });
+// console.log('Starting');
